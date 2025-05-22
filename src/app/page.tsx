2381bdbdab3e0823e04f786848cbaa7ecb0ff28e
Aditya <adi101197sahu@gmail.com>
2025-05-22 16:25:29 +0530

    Hero Section top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@
 
 import Image from "next/image";
 import styles from "./page.module.css";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
      <div className="text-white text-center">MUSICNEST</div>
+     <HeroSection />
       </main>
   );
 }
